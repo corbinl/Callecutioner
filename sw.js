d8298bbf3344e7cbd957b12bd2e36bbfd9a47eca
corbinl <42195582+corbinl@users.noreply.github.com>
2026-07-07 08:31:06 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fx-v3";
+const CACHE = "fx-v4";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
