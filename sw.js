8e08300d16283cfc75470da5727b7b2d9c9a5214
Corbin Lightfoot <corbinlightfoot@gmail.com>
2026-07-11 06:58:36 -0600

    Pin assertive Ben-voice gatekeeper comeback to top of Objections wall, one-time migration for existing users (v1.2.11, sw fx-v14)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fx-v13";
+const CACHE = "fx-v14";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
